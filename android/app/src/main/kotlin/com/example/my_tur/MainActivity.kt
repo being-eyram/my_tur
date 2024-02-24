172ed25c670746b4752925e290602f681e23aa57
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2024-02-24 14:13:13 +0000

    Init commit

---
+++ android/app/src/main/kotlin/com/example/my_tur/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_tur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _launchDialog();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0,
+                ),
+              ),
+              child: Icon(Icons.rocket)),
+        ),
+        title: Text(
+          'My Tur',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/ic_map.png',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.my_library_books_rounded),
+            label: "History",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "Tur explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: "Help",
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future _launchDialog() {
+    return showDialog(
+      context: context,
+      barrierColor: Colors.transparent,
+      builder: (context) {
+        return SimpleDialog(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12)
+          ),
+          backgroundColor: Colors.white,
+          surfaceTintColor: Colors.white,
+          title: Text(
+            "Request/Schedule",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
+          children: [
+            SizedBox(height: 12),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius:
+                      BorderRadius.circular(10), // This rounds the corners
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                hintText: "Enter Service Location",
+                hintStyle: TextStyle(color: Colors.grey),
+                suffixIcon: Icon(Icons.pin_drop),
+              ),
+            ),
+            SizedBox(height: 12),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Summary"),
+                ),
+                Spacer(),
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6)
+                    )
+                  ),
+                  child: Text("Cleaning Option"),
+                )
+              ],
+            ),
+            SizedBox(height: 12),
+          ],
+        );
+      },
+    );
+  }
+}
